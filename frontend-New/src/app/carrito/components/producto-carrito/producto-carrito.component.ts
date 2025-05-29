f6ea38a6381c98e804e62c1b26c284aa6e3339b6
Ax <ajchavarri.23@est.ucab.edu.ve>
2025-05-29 02:20:47 -0400

    mensaje error en carrito coponente
    a

--- frontend-New/src/app/carrito/components/producto-carrito/producto-carrito.component.ts
+++ frontend-New/src/app/carrito/components/producto-carrito/producto-carrito.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, input, OnInit, output, Output } from '@angular/core';
+import {Component, EventEmitter, input, OnInit, output, Output, signal} from '@angular/core';
 import { ProductoCarrito } from '../../../shared/models/producto-carrito';
 import { CartService } from '../../../shared/services/carrito-productos.service';
 import { RouterLink } from '@angular/router';
@@ -14,11 +14,14 @@ import { CurrencyPipe } from '@angular/common';
 export class ProductoCarritoComponent implements OnInit {
   constructor(private carritoService: CartService) {} // Inyecta el servicio
 
+  // Esto hace que angular provea los datos del producto en el carrito
   productoCarrito=input.required<ProductoCarrito>();
   total: number = 0;
 
   // Evento para actualizar el carrito
   updateCartEvent = output<void>();
+
+  mensaje = signal<string>(''); // ✅ Para mostrar el resultado al usuario
 
   ngOnInit(): void {
     this.actualizarTotal()
@@ -34,7 +37,14 @@ export class ProductoCarritoComponent implements OnInit {
   incrementarAlCarrito(): void {
     //Esto se puede cambiar para trabajarlo directamente con el back
     //pero por ahora lo mantendre en localstorage
-    console.log(this.carritoService.addToCart(this.productoCarrito().producto!, 1));
+
+    const result = this.carritoService.addToCart(this.productoCarrito().producto!, 1);
+    // Verifica si el producto tiene stock disponible
+    if (result === 'No hay suficiente stock disponible.') {
+      this.mensaje.set('❌ No hay suficiente stock disponible.');
+      this.limpiarMensaje();
+    }
+
     this.actualizarTotal();
     this.updateCartEvent.emit(); // Emitir el evento para actualizar el carrito
   }
@@ -49,6 +59,11 @@ export class ProductoCarritoComponent implements OnInit {
   removerDelCarrito(): void {
     this.carritoService.removeFromCart(this.productoCarrito().producto!.id);
     this.updateCartEvent.emit(); // Emitir el evento para actualizar el carrito
+  }
+  private limpiarMensaje(): void {
+    setTimeout(() => {
+      this.mensaje.set('');
+    }, 3000); // Limpia el mensaje después de 3 segundos
   }
 
 }
